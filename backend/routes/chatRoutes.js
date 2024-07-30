c8660e41c8b8dfeff77c7cd5d49f6022f9ed83c6
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2024-07-30 15:35:58 +0530

    [Completed] Chat Endpoints

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,14 +4,17 @@ const {
   accessChat,
   fetchChats,
   createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
 } = require("../controllers/chatController");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToroup);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
 
 module.exports = router;
